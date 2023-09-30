007e8bc2086a718f677d96c29ede4774818734c9
theGobindSingh <thesinghgobind@gmail.com>
2023-10-01 00:12:06 +0530

    init

--- app/dist/index.js
+++ app/dist/index.js
@@ -1,10 +1,24 @@
 'use strict';
 
 var express = require('express');
+var httpProxy = require('http-proxy');
 
+var apiProxy = httpProxy.createProxyServer();
+var devHowServer = "http://localhost:3001";
+var howServer = "http://localhost:3000";
+var abhasServer = "http://localhost:3002";
 var app = express();
-app.get("/", function (req, res) {
-    res.send("Hello World!");
+app.use(function (req, res, next) {
+    var hostname = req.hostname;
+    if (hostname.startsWith("dev")) {
+        apiProxy.web(req, res, { target: devHowServer });
+    }
+    else if (hostname.startsWith("abhas")) {
+        apiProxy.web(req, res, { target: abhasServer });
+    }
+    else {
+        apiProxy.web(req, res, { target: howServer });
+    }
 });
 app.listen(1234, function () {
     console.log("server started");
--- app/src/index.ts
+++ app/src/index.ts
@@ -1,9 +1,21 @@
 import express from "express";
+import httpProxy from "http-proxy";
 
+var apiProxy = httpProxy.createProxyServer();
+const devHowServer = "http://localhost:3001";
+const howServer = "http://localhost:3000";
+const abhasServer = "http://localhost:3002";
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use((req, res, next) => {
+  const { hostname } = req;
+  if (hostname.startsWith("dev")) {
+    apiProxy.web(req, res, { target: devHowServer });
+  } else if (hostname.startsWith("abhas")) {
+    apiProxy.web(req, res, { target: abhasServer });
+  } else {
+    apiProxy.web(req, res, { target: howServer });
+  }
 });
 
 app.listen(1234, () => {
